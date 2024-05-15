680a779b54fe3bd950bc04fc94181feb826fe195
songje30 <songje30@naver.com>
2024-05-15 22:50:44 +0900

    경험치, 체력, 피격, 다시시작, 일시정지(메인으로) 완성

--- Mori_Unity_URP/Assets/Undead Survivor/Codes/HUD.cs
+++ Mori_Unity_URP/Assets/Undead Survivor/Codes/HUD.cs
@@ -5,12 +5,18 @@ using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
+
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
     public enum InfoType { Exp, Level, Health, HealthInfo }
     public InfoType type;
 
     Text myText;
     Slider mySlider;
-
     void Awake()
     {
         mySlider = GetComponent<Slider>();
@@ -24,7 +30,7 @@ public class HUD : MonoBehaviour
             case InfoType.Exp:
                 float curExp = GameManager.instance.exp; //현재 경험치
                 float maxExp = GameManager.instance.nextExp[GameManager.instance.level];  //최대 경험치
-                mySlider.value = curExp/maxExp;
+                mySlider.value = curExp/maxExp; //경험치바에 표시
                 break;
 
             case InfoType.Level:
@@ -34,9 +40,9 @@ public class HUD : MonoBehaviour
             case InfoType.Health:
                 float curHealth = GameManager.instance.health; //현재 체력
                 float maxHealth = GameManager.instance.maxHealth;  //최대 체력
-                mySlider.value = curHealth / maxHealth ;
+                mySlider.value = curHealth / maxHealth ; //체력바에 표시
                 break;
-            case InfoType.HealthInfo:
+            case InfoType.HealthInfo://체력바 체력 수치
                 myText.text = myText.text = string.Format("{0:F0}/{1:F0}", GameManager.instance.health, GameManager.instance.maxHealth);
                 break;
         }
