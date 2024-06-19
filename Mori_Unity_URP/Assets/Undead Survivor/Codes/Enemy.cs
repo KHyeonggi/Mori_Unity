0bb10fe9f53971e7ee3450144bbefe772063961a
RamPowerGH <85931677+RamPowerGH@users.noreply.github.com>
2024-06-19 21:28:57 +0900

    피격구현

--- Mori_Unity_URP/Assets/Undead Survivor/Codes/Enemy.cs
+++ Mori_Unity_URP/Assets/Undead Survivor/Codes/Enemy.cs
@@ -15,19 +15,22 @@ public class Enemy : MonoBehaviour
     bool isLive;
 
     Rigidbody2D rigid;
+    Collider2D coll;
     Animator anim;
     SpriteRenderer spriter;
-
+    WaitForFixedUpdate wait;
     void Awake()
     {
+        coll =GetComponent<Collider2D>();
         rigid = GetComponent<Rigidbody2D>();
         anim=GetComponent<Animator>();
         spriter = GetComponent<SpriteRenderer>();
+        wait = new WaitForFixedUpdate();
     }
 
     void FixedUpdate()
     {
-        if (!isLive){
+        if (!isLive|| anim.GetCurrentAnimatorStateInfo(0).IsName("Hit")){
             return;
         }
         float distanceToTarget = Vector2.Distance(rigid.position, target.position);
@@ -52,6 +55,11 @@ public class Enemy : MonoBehaviour
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
         isLive = true;
         health = maxHealth;
+        isLive =true;
+        coll.enabled = true;
+        rigid.simulated=true;
+        spriter.sortingOrder = 2;
+        anim.SetBool("Dead", false);
     }
     public void Init(SpawnData data)
     {
@@ -68,17 +76,32 @@ public class Enemy : MonoBehaviour
             return;
 
         health -= collision.GetComponent<Bullet>().damage;
+        StartCoroutine(KnockBack());
 
         if (health>0)//살았을때
         {
-
+            anim.SetTrigger("Hit");
         }
         else//죽었을때
         {
-            Dead();
+            isLive =false;
+            coll.enabled = false;
+            rigid.simulated=false;
+            spriter.sortingOrder = 1;
+            anim.SetBool("Dead", true);
             GameManager.instance.GetExp();
         }
     }
+
+    IEnumerator KnockBack()
+    {
+        yield return wait;
+
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 dirVec = transform.position - playerPos;
+        rigid.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
+    }
+
     void Dead()
     {
         gameObject.SetActive(false);
