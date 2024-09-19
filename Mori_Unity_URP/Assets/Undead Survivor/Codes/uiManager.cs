1ecdc149a5cf71c276b785da94f46fef7e3b1559
khs1209 <162388872+khs1209@users.noreply.github.com>
2024-09-19 23:14:36 +0900

    name

---
+++ Mori_Unity_URP/Assets/Undead Survivor/Codes/uiManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+    public Text nameText; // 캐릭터 이름을 표시할 Text 컴포넌트
+    public Text talkText; // 대사를 표시할 Text 컴포넌트
+
+    public void SetNameText(string name)
+    {
+        if (nameText != null)
+        {
+            nameText.text = name; // 이름 설정
+        }
+    }
+
+    public void SetTalkText(string talk)
+    {
+        if (talkText != null)
+        {
+            talkText.text = talk; // 대사 설정
+        }
+    }
+}
+
+
