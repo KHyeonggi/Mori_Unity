d6eee4b4761887546a0de68cf90bb520c88d73c2
Hyeonggi <85931677+KHyeonggi@users.noreply.github.com>
2024-12-04 16:21:22 +0900

    start scene 완성

--- Mori_Unity_URP/Assets/Undead Survivor/Codes/MapSelector.cs
+++ Mori_Unity_URP/Assets/Undead Survivor/Codes/MapSelector.cs
@@ -6,14 +6,14 @@ public class MapSelector : MonoBehaviour
     // 스토리 맵 버튼 클릭 시 호출
     public void LoadStoryMap()
     {
-        SceneManager.LoadScene("Demo (1.4)"); // "StoryMapScene" 이름의 씬 로드
-        Debug.Log("stroybutton");
+        Debug.Log("Story Map Button Clicked");
+        SceneManager.LoadScene("Demo (1.4)"); // "Demo (1.4)" 씬 이름이 정확한지 확인
     }
 
     // 랜덤 맵 버튼 클릭 시 호출
     public void LoadRandomMap()
     {
-        SceneManager.LoadScene("RandommapDemo(1.0)"); // "RandomMapScene" 이름의 씬 로드
-        Debug.Log("Random");
+        Debug.Log("Random Map Button Clicked");
+        SceneManager.LoadScene("RandommapDemo(1.0)"); // "RandommapDemo(1.0)" 씬 이름이 정확한지 확인
     }
 }
