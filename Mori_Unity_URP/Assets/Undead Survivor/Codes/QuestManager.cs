dce18a177d59e0e51836f912423bbe081fdb618d
khs1209 <162388872+khs1209@users.noreply.github.com>
2024-12-06 02:42:41 +0900

    바람정령 퀘스트(미완)

--- Mori_Unity_URP/Assets/Undead Survivor/Codes/QuestManager.cs
+++ Mori_Unity_URP/Assets/Undead Survivor/Codes/QuestManager.cs
@@ -32,9 +32,9 @@ public class QuestManager : MonoBehaviour
     {
         questList.Add(10, new QuestData("말을 걸어보자", new int[] { 1000, 1000 })); //10:퀘스트 id  1000:npc id //물정령한테 두번 말걸기
         questList.Add(20, new QuestData("물의 정령의 부탁", new int[] { 5000, 1000 }));//부탁한것(id:5000)과 상호작용뒤 물정령한테 말걸기
-        questList.Add(30, new QuestData("퀘스트 완료 - 물의 정령의 부탁", new int[] { 3000 }));
-        questList.Add(40, new QuestData("바람의 시험", new int[] { 10+3000 ,6000})); 
-        questList.Add(50, new QuestData("퀘스트 완료 - 바람의 시험", new int[] { 0 }));
+        questList.Add(30, new QuestData("퀘스트 완료 - 물의 정령의 부탁", new int[] { 0 }));
+        questList.Add(30, new QuestData("바람의 시험", new int[] { 3000, 6000,3000})); 
+        questList.Add(40, new QuestData("퀘스트 완료 - 바람의 시험", new int[] { 0 }));
     }
     
     public bool IsQuestActive(int questId)
@@ -53,21 +53,23 @@ public class QuestManager : MonoBehaviour
 
         if (id == questList[questId].npcId[questActionIndex])
             questActionIndex++;
+        Debug.Log($"questActionIndex 증가: {questActionIndex}");
 
         ControlObject(); //퀘스트 오브젝트 제어
 
+        // 모든 NPC와 대화가 끝났으면 NextQuest 호출
         if (questActionIndex == questList[questId].npcId.Length)
         {
-            NextQuest();
+            Debug.Log("퀘스트 완료! 다음 퀘스트로 넘어갑니다.");
+            NextQuest(); // 퀘스트 완료 후 다음 퀘스트로 이동
         }
-
-
         return questList[questId].questName;
     }
     void NextQuest()
     {
         questId += 10;
         questActionIndex = 0;
+        Debug.Log("NextQuest 호출: questId = " + questId);
     }
     void ControlObject()
     {
@@ -79,6 +81,15 @@ public class QuestManager : MonoBehaviour
         else if (questId == 20 && questActionIndex == 1)
         {
             questObject[0].SetActive(false); // 물의 정령 오브젝트 비활성화
+        }
+
+        // 바람의 정령 퀘스트 로직
+        if (questId == 30 && questActionIndex == 2)
+        {
+            questObject[0].SetActive(true); 
+        }
+        else if (questId == 30 && questActionIndex == 1)
+        {
         }
     }
 }
